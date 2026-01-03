1cd178fdcf00a26c9a860375f70d4b244eb8cc03
KONFeature <quentin@nivelais.com>
2026-01-03 14:58:45 +0100

    Fine tune local opencode setup

--- packages/php/assets/js/settings.asset.php
+++ packages/php/assets/js/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '91dcf62baba5daa90fd6');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '21b31616e3f8878ff476');
